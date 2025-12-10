5ccdcb063a3c94ccd79f92d409397660fffbc351
Aravind <aravind@hibizsolutions.com>
2025-12-10 16:32:16 +0530

    basic agent

--- basic-agent/greeting_agent/agent.py
+++ basic-agent/greeting_agent/agent.py
@@ -4,9 +4,6 @@ root_agent = Agent(
     name="greeting_agent",
     # https://ai.google.dev/gemini-api/docs/models
     model="gemini-2.0-flash",
-    description="Greeting agent",
-    instruction="""
-    You are a helpful assistant that greets the user. 
-    Ask for the user's name and greet them by name.
-    """,
+    description="A helpful assistant for user questions.",
+    instruction='Answer user questions to the best of your knowledge',
 )
